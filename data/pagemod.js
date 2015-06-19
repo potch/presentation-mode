a9a7e1be038edef563203ef0e70d3c94358b1673
Matt Claypotch <thepotch@gmail.com>
2015-06-19 10:47:52 -0700

    For Ross!

---
+++ data/pagemod.js
@@ -0,0 +1,22 @@
+(function () {
+  function toggleFullScreen() {
+    if (window.fullScreen) {
+      document.body.mozCancelFullScreen();
+    } else {
+      document.body.mozRequestFullScreen();
+    }
+  }
+
+  var menu = document.createElement('menu');
+  var item = document.createElement('menuitem');
+
+  menu.setAttribute('id', 'fsmenu');
+  menu.setAttribute('type', 'context');
+
+  item.setAttribute('label', 'Presentation Mode');
+  item.addEventListener('click', toggleFullScreen);
+
+  menu.appendChild(item);
+  document.body.appendChild(menu);
+  document.body.setAttribute('contextmenu', 'fsmenu');
+})();
